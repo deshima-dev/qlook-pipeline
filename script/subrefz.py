52ca391bc27236ee1c26cc83f1a3a71b562064db
Tsuyoshi Ishida <tishida@ioa.s.u-tokyo.ac.jp>
2020-10-15 21:57:55 +0900

    refactoring subrefz.py

--- script/subrefz.py
+++ script/subrefz.py
@@ -19,46 +19,35 @@ from astropy.io import fits
 from astropy.modeling import models, fitting
 
 # original package
-import functions as fc
+from . import functions as fc
 
 
 ##### command line arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('dfitsname', help='DFITS name')
-parser.add_argument('antname', help='antenna log')
-parser.add_argument('ymlname', help='parameter file')
+parser.add_argument('dfits_file', help='DFITS name')
+parser.add_argument('antlog_file', help='antenna log')
+parser.add_argument('yaml_file', help='parameter file')
 args = parser.parse_args()
 
-dfitsname = pathlib.Path(args.dfitsname)
-antname   = pathlib.Path(args.antname)
-ymlname   = pathlib.Path(args.ymlname)
-with open(ymlname) as file:
-    ymldata = yaml.load(file, Loader=yaml.SafeLoader)
+dfits_file  = pathlib.Path(args.dfits_file)
+antlog_file = pathlib.Path(args.antlog_file)
+yaml_file   = pathlib.Path(args.yaml_file)
+with open(yaml_file) as file:
+    params = yaml.load(file, Loader=yaml.SafeLoader)
 
 ##### directory settings
-obsid  = dfitsname.name.split('_')[1].split('.')[0]
-outdir = pathlib.Path(ymldata['file']['outdir']) / obsid
-if not outdir.exists():
-    outdir.mkdir(parents=True)
-fitdir = outdir / 'fit'
-if not fitdir.exists():
-    fitdir.mkdir()
-outtxt = outdir / ymldata['file']['outtxt']
-imgfmt = ymldata['file']['imgfmt']
-pltflg = ymldata['file']['pltflg']
+obsid      = dfits_file.name.split('_')[1].split('.')[0]
+output_dir = pathlib.Path(params['file']['output_dir']) / obsid
+if not output_dir.exists():
+    output_dir.mkdir(parents=True)
+result_file  = output_dir / params['file']['result_file']
+image_format = params['file']['image_format']
+do_plot      = params['file']['do_plot']
+dpi          = params['file']['dpi']
 
 ##### fc.loaddfits parameters
-coordtype = ymldata['loaddfits']['coordtype']
-mode      = ymldata['loaddfits']['mode']
-loadtype  = ymldata['loaddfits']['loadtype']
-
-array = fc.loaddfits(
-            dfitsname,
-            antname,
-            coordtype=coordtype,
-            mode=mode,
-            loadtype=loadtype
-        )
+ch = params['loaddfits']['ch']
+array = fc.loaddfits(dfits_file, antlog_file, **params['loaddfits'])
 
 ##### 1st step: check time stream
 print('#1: check time stream')
@@ -66,29 +55,24 @@ print('#1: check time stream')
 scantypes = np.unique(array.scantype)
 print(f'scantypes: {scantypes}')
 
-fig, ax = plt.subplots(2, 1, figsize=(10, 5))
-tstart0 = ymldata['check_scantypes']['tstart0']
-tend0   = ymldata['check_scantypes']['tend0']
-tstart1 = ymldata['check_scantypes']['tstart1']
-tend1   = ymldata['check_scantypes']['tend1']
+fig, ax = plt.subplots(2, 1, figsize=(10, 5), dpi=dpi)
+tstart0 = params['check_scantypes']['tstart0']
+tend0   = params['check_scantypes']['tend0']
+tstart1 = params['check_scantypes']['tstart1']
+tend1   = params['check_scantypes']['tend1']
 subarray0 = array[tstart0:tend0, :]
 subarray1 = array[tstart1:tend1, :]
 
-refch  = ymldata['check_scantypes']['refch']
-xtick  = 'time'
-marker = '.'
-linestyle  = 'None'
+refch = params['check_scantypes']['refch']
+plot_params = {'marker': '.', 'markersize': 0.5, 'linestyle': 'None'}
 
-dc.plot.plot_timestream(subarray0, kidid=refch, xtick=xtick, scantypes=['GRAD'], ax=ax[0],
-                        marker=marker, linestyle=linestyle)
-dc.plot.plot_timestream(subarray0, kidid=refch, xtick=xtick, scantypes=['ON'], ax=ax[0],
-                        marker=marker, linestyle=linestyle)
-dc.plot.plot_timestream(subarray1, kidid=refch, xtick=xtick, scantypes=['ON'], ax=ax[1],
-                        marker=marker, linestyle=linestyle)
+dc.plot.plot_timestream(subarray0, ch, scantypes=['GRAD'], ax=ax[0], **plot_params)
+dc.plot.plot_timestream(subarray0, ch, scantypes=['ON'], ax=ax[0], **plot_params)
+dc.plot.plot_timestream(subarray1, ch, scantypes=['ON'], ax=ax[1], **plot_params)
 
 fig.tight_layout()
-fig.savefig(outdir / f'timestream_{obsid}.{imgfmt}')
-if pltflg:
+fig.savefig(output_dir / f'time_stream.{image_format}')
+if do_plot:
     plt.show()
 else:
     plt.clf()
@@ -97,24 +81,24 @@ else:
 ##### 2nd step: plot subref offsets vs time
 print('#2: plot subref offsets vs time')
 
-fig, ax = plt.subplots(3, 1, figsize=(10, 7))
-dc.plot.plot_tcoords(array, coords=('time', 'subref_x') ,scantypes=['ON'], ax=ax[0])
-dc.plot.plot_tcoords(array, coords=('time', 'subref_y') ,scantypes=['ON'], ax=ax[1])
-dc.plot.plot_tcoords(array, coords=('time', 'subref_z') ,scantypes=['ON'], ax=ax[2])
+fig, ax = plt.subplots(3, 1, figsize=(10, 7), dpi=dpi)
+dc.plot.plot_tcoords(array, ('time', 'subref_x'), scantypes=['ON'], ax=ax[0])
+dc.plot.plot_tcoords(array, ('time', 'subref_y'), scantypes=['ON'], ax=ax[1])
+dc.plot.plot_tcoords(array, ('time', 'subref_z'), scantypes=['ON'], ax=ax[2])
 
-maxid = list(argrelmax(array.subref_z[array.scantype == 'ON'].values, order=1)[0])
-minid = list(argrelmin(array.subref_z[array.scantype == 'ON'].values, order=1)[0])
-ax[2].plot(array.time[array.scantype == 'ON'][maxid],
-           array.subref_z[array.scantype == 'ON'][maxid],
+maxid = list(argrelmax(array.subref_z[array.scantype=='ON'].values, order=1)[0])
+minid = list(argrelmin(array.subref_z[array.scantype=='ON'].values, order=1)[0])
+ax[2].plot(array.time[array.scantype=='ON'][maxid],
+           array.subref_z[array.scantype=='ON'][maxid],
            'o', color='C1', label='local max')
-ax[2].plot(array.time[array.scantype == 'ON'][minid],
-           array.subref_z[array.scantype == 'ON'][minid],
+ax[2].plot(array.time[array.scantype=='ON'][minid],
+           array.subref_z[array.scantype=='ON'][minid],
            'o', color='C2', label='local min')
 ax[2].legend()
 
 fig.tight_layout()
-fig.savefig(outdir / f'subrefxyz_vs_time_{obsid}.{imgfmt}')
-if pltflg:
+fig.savefig(output_dir / f'subref_movement.{image_format}')
+if do_plot:
     plt.show()
 else:
     plt.clf()
@@ -123,16 +107,16 @@ else:
 ##### 3rd step: plot temperature vs subref_z
 print('#3: plot temperature vs subref_z')
 
-fig, ax = plt.subplots(1, 1, figsize=(10, 5))
-ax.plot(array.subref_z[array.scantype == 'ON'], array[:, refch][array.scantype == 'ON'], label='ON')
+fig, ax = plt.subplots(1, 1, figsize=(10, 5), dpi=dpi)
+ax.plot(array.subref_z[array.scantype=='ON'], array[:, ch][array.scantype=='ON'], label='ON')
 ax.set_xlabel('subref_z')
 ax.set_ylabel('temperature')
-ax.set_title(f'ch #{refch}')
+ax.set_title(f'ch #{ch}')
 ax.legend()
 
 fig.tight_layout()
-fig.savefig(outdir / f'temp_vs_subrefz_{obsid}.{imgfmt}')
-if pltflg:
+fig.savefig(output_dir / f'temp_vs_subrefz.{image_format}')
+if do_plot:
     plt.show()
 else:
     plt.clf()
@@ -148,11 +132,11 @@ if maxid[0] < minid[0]:
 if minid[-1] < maxid[-1]:
     minid.append(np.nan)
 
-amp0 = ymldata['fitting']['amplitude']
-z0   = ymldata['fitting']['z_mean']
-s0   = ymldata['fitting']['z_stddev']
-sl   = ymldata['fitting']['slope']
-ic   = ymldata['fitting']['intercept']
+amp0 = params['fitting']['amplitude']
+z0   = params['fitting']['z_mean']
+s0   = params['fitting']['z_stddev']
+sl   = params['fitting']['slope']
+ic   = params['fitting']['intercept']
 
 g_init = models.Gaussian1D(amplitude=amp0, mean=z0, stddev=s0) + models.Linear1D(sl, ic)
 fit_g  = fitting.LevMarLSQFitter()
@@ -170,12 +154,12 @@ for n in range(len(maxid)):
             ax.plot(array.subref_z[array.scantype == 'ON'][minid[n]:maxid[n]],
                     g(array.subref_z[array.scantype == 'ON'][minid[n]:maxid[n]]),
                     color='C2', label='model (dZ > 0)')
-        
+
             dt = (array.time[array.scantype == 'ON'][maxid[n]] - array.time[array.scantype == 'ON'][minid[n]]).values.item() / 1e9
             ds = float((array.subref_z[array.scantype == 'ON'][maxid[n]] - array.subref_z[array.scantype == 'ON'][minid[n]]).values)
             ss = ds / dt
             alldata.add_row((ss, g.amplitude_0, g.mean_0, g.stddev_0, g.slope_1, g.intercept_1))
-        
+
         ax.plot(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 array[:, refch][array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 color='C1', label='obs (dZ < 0)')
@@ -185,12 +169,12 @@ for n in range(len(maxid)):
         ax.plot(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 g(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]]),
                 color='C3', label='model (dZ < 0)')
-        
+
         dt = (array.time[array.scantype == 'ON'][minid[n+1]] - array.time[array.scantype == 'ON'][maxid[n]]).values.item() / 1e9
         ds = float((array.subref_z[array.scantype == 'ON'][minid[n+1]] - array.subref_z[array.scantype == 'ON'][maxid[n]]).values)
         ss = ds / dt
         alldata.add_row((ss, g.amplitude_0, g.mean_0, g.stddev_0, g.slope_1, g.intercept_1))
-    
+
     elif n == len(maxid) - 1:
         ax.plot(array.subref_z[array.scantype == 'ON'][minid[n]:maxid[n]],
                 array[:, refch][array.scantype == 'ON'][minid[n]:maxid[n]],
@@ -217,7 +201,7 @@ for n in range(len(maxid)):
             ax.plot(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]],
                     g(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]]),
                     color='C3', label='model (dZ < 0)')
-            
+
             dt = (array.time[array.scantype == 'ON'][minid[n+1]] - array.time[array.scantype == 'ON'][maxid[n]]).values.item() / 1e9
             ds = float((array.subref_z[array.scantype == 'ON'][minid[n+1]] - array.subref_z[array.scantype == 'ON'][maxid[n]]).values)
             ss = ds / dt
@@ -232,12 +216,12 @@ for n in range(len(maxid)):
         ax.plot(array.subref_z[array.scantype == 'ON'][minid[n]:maxid[n]],
                 g(array.subref_z[array.scantype == 'ON'][minid[n]:maxid[n]]),
                 color='C2', label='model (dZ > 0)')
-        
+
         dt = (array.time[array.scantype == 'ON'][maxid[n]] - array.time[array.scantype == 'ON'][minid[n]]).values.item() / 1e9
         ds = float((array.subref_z[array.scantype == 'ON'][maxid[n]] - array.subref_z[array.scantype == 'ON'][minid[n]]).values)
         ss = ds / dt
         alldata.add_row((ss, g.amplitude_0, g.mean_0, g.stddev_0, g.slope_1, g.intercept_1))
-        
+
         ax.plot(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 array[:, refch][array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 color='C1', label='obs (dZ < 0)')
@@ -247,7 +231,7 @@ for n in range(len(maxid)):
         ax.plot(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]],
                 g(array.subref_z[array.scantype == 'ON'][maxid[n]:minid[n+1]]),
                 color='C3', label='model (dZ < 0)')
-        
+
         dt = (array.time[array.scantype == 'ON'][minid[n+1]] - array.time[array.scantype == 'ON'][maxid[n]]).values.item() / 1e9
         ds = float((array.subref_z[array.scantype == 'ON'][minid[n+1]] - array.subref_z[array.scantype == 'ON'][maxid[n]]).values)
         ss = ds / dt
@@ -259,8 +243,8 @@ for n in range(len(maxid)):
     ax.legend()
 
     fig.tight_layout()
-    fig.savefig(fitdir / f'subrefz_fit_#{n}_{obsid}.{imgfmt}')
-    if pltflg:
+    fig.savefig(output_dir / f'subrefz_fit_#{n}.{image_format}')
+    if do_plot:
         plt.show()
     else:
         plt.clf()
@@ -286,11 +270,11 @@ ax.set_ylabel('subref_z')
 ax.legend()
 
 fig.tight_layout()
-fig.savefig(outdir / f'subrefz_vs_scanspeed_{obsid}.{imgfmt}')
-if pltflg:
-        plt.show()
+fig.savefig(output_dir / f'subrefz_vs_scanspeed.{image_format}')
+if do_plot:
+    plt.show()
 else:
     plt.clf()
     plt.close()
 
-alldata.write(outtxt, format='ascii', overwrite=True)
+alldata.write(result_file, format='ascii', overwrite=True)
